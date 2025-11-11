f4de18d445c79e7997208a8a07f2c476353fc520
ÜzeyirCan <140662384+uzeyircan@users.noreply.github.com>
2025-11-11 22:08:28 +0300

    trailer

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TextInput, Pressable } from "react-native";
+import { useState } from "react";
 
 export default function App() {
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [result, setResult] = useState("");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome {result}</Text>
+      <Text>Name</Text>
+      <TextInput
+        placeholder="Enter Your Name "
+        style={styles.textInputStyle}
+        onChangeText={setName}
+        value={name}
+      />
+      <Text>Last Name</Text>
+      <TextInput
+        placeholder="Enter Your Last Name"
+        style={styles.textInputStyle}
+        onChangeText={setLastName}
+        value={lastName}
+      />
+      <Pressable
+        onPress={() => setResult(name + " " + lastName)}
+        style={({ pressed }) => [
+          {
+            backgroundColor: pressed ? "gray" : "lightblue",
+          },
+          styles.button,
+        ]}
+      >
+        <Text style={styles.buttonText}>Save</Text>
+      </Pressable>
     </View>
   );
 }
@@ -17,7 +46,23 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  subContainer: {
-    backgroundColor: "black",
+  textInputStyle: {
+    borderWidth: 1,
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    marginVertical: 10,
+  },
+  button: {
+    borderWidth: 1,
+    width: "80%",
+    height: 50,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    color: "white",
   },
 });
